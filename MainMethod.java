9e090aad84515e911589e08b60f3553193ab7df0
Jeelans <102316773+Jeelans@users.noreply.github.com>
2022-03-24 22:48:24 +0530

    Add files via upload

---
+++ MainMethod.java
@@ -0,0 +1,12 @@
+package LockedMe;
+
+public class MainMethod {
+		public static final String path = "C:/Users/91799/Desktop/proj";
+		public static void main(String[] args) {
+			Menus menu = new Menus();
+			menu.introScreen();
+			menu.mainMenu();
+
+	}
+
+}
